957127eb83ef3fbc3589f00b439300d9d67e2628
Mangesh Balkawade <101330511+mangesh-balkawade@users.noreply.github.com>
2022-06-07 12:50:55 +0530

    Components Creation and Rendering

---
+++ src components/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
