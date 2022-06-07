c5a9749a2b5060ec26dc4fd8695d12cb83347b66
Mangesh Balkawade <101330511+mangesh-balkawade@users.noreply.github.com>
2022-06-07 12:07:37 +0530

    Event And Style Binding
    This is the Source code Events and Style Bindings in Angular.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Asm6'`, () => {
+  it(`should have as title 'Asm7'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Asm6');
+    expect(app.title).toEqual('Asm7');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Asm6 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Asm7 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-//   template:`<div>
-//   <label for="textfiels">TextField : </label>
-//   <input type="text" id="textfiels" placeholder="enter text"><br>
-//   </div>
-//   <h1>Marvellous Infosystem</h1>`,
-//   styles:[
-//    ` h1
-// {
-//     text-align: center;
-//     color: blue;
-// }
-// div
-// {
-//     margin-left: auto;
-//     margin-right: auto;
-//     text-align: center;
-// }
-// `
-//   ]
-templateUrl:'app.component.html',
-styleUrls:['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  str:string="Marvellous Infosystem"
+ 
+  public fun()
+  {
+    this.str = "Education For Better Tommorrow";
+  }
+
+  public upperCase()
+  {
+    this.str=this.str.toUpperCase();
+  }
+
+  public lowerCase()
+  {
+    this.str=this.str.toLowerCase();
+  }
 }
